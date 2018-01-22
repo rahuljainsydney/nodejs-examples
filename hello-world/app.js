32f79e93bf2cea64c0eadf928fefcd8d38ae1dc6
COCHLEAR\rjain <rjain@cochlear.com>
2018-01-22 16:30:59 +1100

    move git ignore

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,8 +2,13 @@ console.log('starting app.js')
 
 const fs=require('fs');
 const os=require('os');
+const _=require('lodash');
 const notes=require('./notes.js');
 
 var user=os.userInfo();
 console.log(notes.addNote(1,2));
+
+//example of loadash
+var filteredArray=_.uniq(['Rahul',1,2,2,3,4,'Rahul','Test nodemon'])
+console.log(filteredArray);
 //fs.appendFile('greeting.txt',`Hello ${user.username}!`);
