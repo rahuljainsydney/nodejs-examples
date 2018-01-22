194b5ef64e41b630a61dcb75736fd129c9d976f4
COCHLEAR\rjain <rjain@cochlear.com>
2018-01-22 16:29:08 +1100

    add new examples

---
+++ example-2/app.js
@@ -0,0 +1,5 @@
+const fs=require('fs');
+const _=require('lodash');
+const notes=require('./notes');
+
+console.log(process.argv);
---
+++ example-2/notes.js
@@ -0,0 +1,6 @@
+console.log('Starting  notes.js')
+
+module.exports.addNote = (a,b)=>{
+  console.log('addNote');
+  return a+b;
+};
