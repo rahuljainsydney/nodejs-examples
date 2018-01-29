eed40ffca573500cfb7424a72f0580e33af9278a
COCHLEAR\rjain <rjain@cochlear.com>
2018-01-30 09:57:09 +1100

    check in new changes

--- example-2/app.js
+++ example-2/app.js
@@ -1,5 +1,35 @@
-const fs=require('fs');
-const _=require('lodash');
-const notes=require('./notes');
+const fs = require('fs');
+const _ = require('lodash');
+const notes = require('./notes');
+const yargs = require('yargs');
 
-console.log(process.argv);
+//run the app like node app add
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('yargs:', argv);
+
+if (command === 'add') {
+  console.log('adding new note');
+  var note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('note created');
+    notes.logNote(note);
+  } else {
+    console.log('Unable to create note');
+  }
+} else if (command === 'list') {
+  console.log('Listing all notes');
+  notes.getAll();
+} else if (command === 'read') {
+  var note = notes.getNote(argv.title);
+  if (note) {
+    notes.logNote(note);
+  }
+} else if (command === 'remove') {
+  console.log('Removing the title');
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'Note was removed' : 'Note does not exist and not removed';
+  console.log(message);
+} else {
+  console.log('command not recognised');
+}
--- example-2/notes.js
+++ example-2/notes.js
@@ -1,6 +1,71 @@
 console.log('Starting  notes.js')
+const fs = require('fs');
 
-module.exports.addNote = (a,b)=>{
-  console.log('addNote');
-  return a+b;
+//Read the data from the file system
+var fetchNotes = () => {
+  try {
+    var noteString = fs.readFileSync('notes-data.json');
+    return JSON.parse(noteString);
+  } catch (e) {
+    return [];
+  }
 };
+
+//Write data to the file stored in local file system
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+//Adding a new Note to the file, check for duplicates if record exist
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  //check for duplicate in the file
+  var duplicateNotes = notes.filter((note) => note.title === title);
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  console.log('getting all notes');
+}
+
+//Retrieve the note
+var getNote = (title) => {
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
+  //console.log('filtered Note ', filteredNotes[0].body);
+}
+
+var removeNote = (title) => {
+  //fetch notes
+  var notes = fetchNotes();
+  //filter notes, removing the one with the title of arguement
+  var filteredNotes = notes.filter((note) => note.title != title);
+  //save new notes array
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
+}
+
+//reusable function for comments
+var logNote = ((note) => {
+  //add a break (debugger), use node inspect to run in the debugger
+  debugger;
+  console.log('--');
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
+});
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+}
